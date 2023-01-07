6078ae9c518cb23a8b470bda9fe51880ac36a7bc
Abhi Jain <devabhi@amazon.com>
2023-01-07 18:21:57 +0530

    Add get videos pagenated API

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const { fetchVideos } = require('./fetchVideos')
+const { paginatedResults } = require('./dbOps')
 const app = express()
 const port = 3000
 
 fetchVideos()
+
+app.get('/videos', paginatedResults(), (req, res) => {
+    res.json(res.paginatedResults)
+})
 
 app.listen(port, () => {
     console.log(`Youtube search listening on port ${port}`)
